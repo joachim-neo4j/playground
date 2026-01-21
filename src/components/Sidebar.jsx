04a6f655898926feb0c2ca010947c91ccd11f5e6
Joachim Löfstedt <joalof@MacBook-Pro-som-tillhor-Joachim.local>
2026-01-21 09:45:25 +0100

    Replace Developer hub icon with new code/document SVG

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -43,7 +43,9 @@ export default function Sidebar() {
               <span>Get started</span>
             </Link>
             <Link to="/developer-hub" className={`relative flex items-center space-x-3 rounded-md px-3 text-sm font-medium ${isActive('developer-hub')}`} style={{ height: '32px' }}>
-              <CodeBracketIconOutline className="h-5 w-5" />
+              <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m6.75 7.5 3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0 0 21 18V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v12a2.25 2.25 0 0 0 2.25 2.25Z" />
+              </svg>
               <span>Developer hub</span>
             </Link>
             <div 
