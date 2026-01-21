2909f51cabcf5e772d25d1b44c9ab873582b5f79
Joachim Löfstedt <joalof@MacBook-Pro-som-tillhor-Joachim.local>
2026-01-21 11:05:18 +0100

    Change Query icon stroke-width to 1.6

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -141,7 +141,7 @@ export default function Sidebar() {
           </Link>
           <Link to="/query" className={`relative flex items-center space-x-3 rounded-md px-3 text-sm font-medium ${isActive('query')}`} style={{ height: '32px' }}>
             <svg className="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.6" d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
             </svg>
             <span>Query</span>
           </Link>
