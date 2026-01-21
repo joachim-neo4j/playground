af9f1acb9d67293d26e58a1e7074aa8b9cc335d1
Joachim Löfstedt <joalof@MacBook-Pro-som-tillhor-Joachim.local>
2026-01-21 09:32:00 +0100

    Make page use all available vertical space

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,15 +4,15 @@ import StatusBar from './StatusBar';
 
 export default function Layout({ children }) {
   return (
-    <>
+    <div className="flex flex-col h-full">
       <Header />
-      <div className="flex">
+      <div className="flex flex-1 min-h-0">
         <Sidebar />
-        <main className="flex-1 bg-content-area">
+        <main className="flex-1 bg-content-area overflow-auto">
           <StatusBar />
           {children}
         </main>
       </div>
-    </>
+    </div>
   );
 }
